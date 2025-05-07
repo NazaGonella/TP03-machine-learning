b9ab7a4e0cfc5fb1f5fb5d4f451e9f886623a1c7
Naza Gonella <nazagonella2@gmail.com>
2025-05-07 11:37:02 -0300

    backpropagation hecho?

--- src/models.py
+++ src/models.py
@@ -31,8 +31,10 @@ class RedNeuronal:
             pass
         return e_x / e_x.sum(axis=0)
     
-    def cross_entropy(self, y_true : np.ndarray, y_pred : np.ndarray) -> float:
-        return y_true.T @ np.log(y_pred)
+    def cross_entropy(self, y_true : np.ndarray, y_pred : np.ndarray, gradient : bool = False) -> float:
+        if gradient:
+            return - (y_true / y_pred)
+        return - (y_true.T @ np.log(y_pred))
 
     def backpropagation(self, x_i : np.ndarray, y_i : np.ndarray, W : np.ndarray, w_0 : np.ndarray) -> np.ndarray:
         # forward pass
@@ -45,16 +47,22 @@ class RedNeuronal:
         loss_i : float = self.cross_entropy(y_i, pred)
 
         # Backward-pass
-        delta : float = self.h[l](self.a[l], True)
-        al_aw : float = delta @ self.z[self.L-1].T
-        for l in range(1, self.L - 1, -1):
-            pass
+        delta : np.ndarray = np.array([0 for _ in range(self.L)])
+        delta[self.L] = [self.h[l](self.a[l], True)] * self.cross_entropy(y_i, pred, gradient=True)
+        al_aw : np.ndarray = delta @ self.z[self.L - 1].T
+        al_aw0 : np.ndarray = np.array([])
+        for l in range(1, self.L - 1 + 1, -1):
+            delta[l] = self.h[l](self.a[k], True) * (self.w[l+1].T @ delta[l+1])
+            al_aw = delta[l] @ self.z[l-1].T
+            al_aw0 = delta[l]
         
+        return pred, loss_i, al_aw, al_aw0
+
 
     def train(self, x, y) -> None:
         pass
 
-red = RedNeuronal([2, 7, 5], initial_weights=1, F=['relu', 'relu'])
+red = RedNeuronal([2, 7, 5], [RedNeuronal.relu, RedNeuronal.relu])
 print(red.L)
 print(red.M)
 print(red.z)
@@ -63,3 +71,4 @@ print(red.z)
 
 # x = np.array([0.3, 0.5, 0.2, 4, 0.1])
 # print(red.softmax(red.w[2]).T @ x)
+print(list(range(10, 1, -1)))
