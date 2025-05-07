06dfb026209012dcb781e42896ec68528ee87cd9
Naza Gonella <nazagonella2@gmail.com>
2025-05-06 23:39:21 -0300

    checkpoint

--- src/models.py
+++ src/models.py
@@ -2,30 +2,62 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
+from collections.abc import Callable
 
 class RedNeuronal:
-    def __init__(self, M : list[int], initial_weights : int = 0):
-        self.L = len(M)
+    ## M: estructura sin input layer
+    def __init__(
+            self, 
+            M : list[int], 
+            h : list[Callable[[np.ndarray, bool], float]], 
+        ):
         self.M = M
-        self.w = [np.array([initial_weights for _ in range(M[i])], dtype=float) for i in range(len(M))]
-
-    def relu(self, x : float) -> float:
-        return max(0, x)
+        self.h = h
+        self.L = len(M)
+        self.z = [np.array(
+            [0 for _ in range(M[i])], dtype=float
+        ) for i in range(len(M))] # Falta layer de input en z[0]
+        self.a = np.array([]) # len(M) vectores
+        self.w = np.array([]) # len(M) matrices de pesos, I en w[0]
     
-    def softmax(self, x : np.ndarray[float]) -> np.float32:
-        e_x : np.ndarray[float] = np.exp(x - np.max(x))
+    def relu(self, x : np.ndarray, gradient : bool = False) -> np.ndarray:
+        if gradient:
+            return np.where(x > 0, 1, 0)
+        return np.where(x > 0, x, 0)
+    
+    def softmax(self, x : np.ndarray, gradient : bool = False) -> np.ndarray:
+        e_x : np.ndarray = np.exp(x - np.max(x))
+        if gradient:
+            pass
         return e_x / e_x.sum(axis=0)
     
-    def cross_entropy(self, y_true : np.ndarray[float], y_pred : np.ndarray[float]) -> float:
-        return y_true.T @ np.log(y_pred) 
-    
+    def cross_entropy(self, y_true : np.ndarray, y_pred : np.ndarray) -> float:
+        return y_true.T @ np.log(y_pred)
+
+    def backpropagation(self, x_i : np.ndarray, y_i : np.ndarray, W : np.ndarray, w_0 : np.ndarray) -> np.ndarray:
+        # forward pass
+        self.z[0] = x_i
+        for l in range(1, self.L + 1):
+            self.a[l] = W[l] @ self.z[l-1] + w_0[l]
+            self.z[l] = self.h[l](self.a[l])
+        
+        pred : np.ndarray = self.z[self.L]
+        loss_i : float = self.cross_entropy(y_i, pred)
+
+        # Backward-pass
+        delta : float = self.h[l](self.a[l], True)
+        al_aw : float = delta @ self.z[self.L-1].T
+        for l in range(1, self.L - 1, -1):
+            pass
+        
+
     def train(self, x, y) -> None:
         pass
 
-red = RedNeuronal([2, 7, 5], initial_weights=1)
+red = RedNeuronal([2, 7, 5], initial_weights=1, F=['relu', 'relu'])
 print(red.L)
 print(red.M)
-print(red.w)
+print(red.z)
 # print("")
 # print(red.softmax(red.w[2]))
 
